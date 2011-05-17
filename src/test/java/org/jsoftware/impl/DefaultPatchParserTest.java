d7f3c4960ed638e69c45e8d2925ae5d9a1b77563
Marek Szalik <szalik@jsoftware.org>
2011-05-17 10:42:16 +0000

    merge 229:231 from svn://jsoftware.org/grucha/dbpatch-maven-plugin/branches/dbpatch-1.4

--- src/test/java/org/jsoftware/impl/DefaultPatchParserTest.java
+++ src/test/java/org/jsoftware/impl/DefaultPatchParserTest.java
@@ -1,11 +1,11 @@
 package org.jsoftware.impl;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
 
 import junit.framework.Assert;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -21,13 +21,13 @@ public class DefaultPatchParserTest {
 		return c;
 	}
 	
-	@Test @Ignore
+	@Test 
 	public void parse() throws IOException {
 		DefaultPatchParser parser = new DefaultPatchParser();
 		List<PatchStatement> list = parser.parse(getClass().getResourceAsStream("input.txt"), null).getStatements();
-		for(PatchStatement ps : list) {
-			System.out.println(ps.isExecutable() + "  " + ps.toString());
-		}
+//		for(PatchStatement ps : list) {
+//			System.out.println(ps.isExecutable() + "  " + ps.toString());
+//		}
 		
 		Assert.assertEquals(6, countStatements(list, true));
 		Assert.assertEquals(4, countStatements(list, false));
@@ -38,12 +38,23 @@ public class DefaultPatchParserTest {
 	public void parseBlock() throws IOException {
 		DefaultPatchParser parser = new DefaultPatchParser();
 		List<PatchStatement> list = parser.parse(getClass().getResourceAsStream("input-block.txt"), null).getStatements();
-		for(PatchStatement ps : list) {
-			System.out.println(ps.isExecutable() + "  " + ps.toString());
-		}
+//		for(PatchStatement ps : list) {
+//			System.out.println(ps.isExecutable() + "  " + ps.toString());
+//		}
 		Assert.assertEquals(4, countStatements(list, true));
 		Assert.assertEquals(3, countStatements(list, false));
 	}
 	
+	@Test 
+	public void parseSemicon() throws IOException {
+		DefaultPatchParser parser = new DefaultPatchParser();
+		ByteArrayInputStream bais = new ByteArrayInputStream("INSERT INTO a (\"ab;c\\\"d\", 'za;p\\'o')".getBytes());
+		List<PatchStatement> list = parser.parse(bais, null).getStatements();
+//		for(PatchStatement ps : list) {
+//			System.out.println(ps.isExecutable() + "  " + ps.toString());
+//		}
+		Assert.assertEquals(1, countStatements(list, true));
+		Assert.assertEquals("INSERT INTO a (\"ab;c\\\"d\", 'za;p\\'o')", list.get(0).getCode());
+	}
 	
 }
