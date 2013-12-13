3539a879eba311a10a43f3fc28aca3655392cb3b
Marek Szalik <szalik@jsoftware.org>
2013-12-13 12:41:12 +0000

    #868: Add support for System.properties in configuration file (dbpatch.properties)

--- src/main/java/org/jsoftware/config/PropertiesConfigurationParser.java
+++ src/main/java/org/jsoftware/config/PropertiesConfigurationParser.java
@@ -1,5 +1,6 @@
 package org.jsoftware.config;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.jsoftware.config.dialect.DefaultDialect;
 import org.jsoftware.config.dialect.DialectFinder;
 import org.jsoftware.impl.DirectoryPatchScanner;
@@ -12,6 +13,8 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class PropertiesConfigurationParser extends AbstractConfigurationParser {
@@ -20,6 +23,7 @@ public class PropertiesConfigurationParser extends AbstractConfigurationParser {
 	public Collection<ConfigurationEntry> parse(InputStream input) throws IOException, ParseException {
 		Properties p = new Properties();
 		p.load(input);
+        evaluatePropertyValuesInternal(p);
 		Map<String,ConfigurationEntry> confs = new HashMap<String,ConfigurationEntry>();
 		for(Map.Entry<Object,Object> me : p.entrySet()) {
 			String[] keys = me.getKey().toString().toLowerCase().split("\\.");
@@ -64,7 +68,7 @@ public class PropertiesConfigurationParser extends AbstractConfigurationParser {
 				if (value.startsWith("dir")) scanner = new DirectoryPatchScanner();
 				if (value.startsWith("name")) scanner = new NamePatchScanner();
 				if (scanner == null) {
-					throw new ParseException("Unknow scanner type - " + value, 0);
+					throw new ParseException("Unknown scanner type - " + value, 0);
 				} 
 				ce.setPatchScanner(scanner);
 			}
@@ -72,4 +76,46 @@ public class PropertiesConfigurationParser extends AbstractConfigurationParser {
 		return confs.values();
 	}
 
+
+    static int evaluatePropertyValuesInternal(Properties props) {
+        final Pattern pattern = Pattern.compile("(#|\\$)\\{[^\\}]*\\}");
+        int replaceCount = 0;
+        // validation
+        for (Map.Entry<Object, Object> me : props.entrySet()) {
+            if (me.getKey() == null || me.getValue() == null) {
+                continue;
+            }
+            String input = me.getValue().toString();
+            Matcher matcher = pattern.matcher(input);
+            String result = input;
+            int offset = 0;
+            while (matcher.find(offset)) {
+                offset = matcher.start() +1;
+                String group = matcher.group();
+                String replacement = group.substring(2, group.length() - 1);
+                if (replacement.length() > 0) {
+                    String nv = System.getProperty(replacement);
+                    if (nv == null) {
+                        nv = System.getenv(replacement);
+                    }
+                    if (nv == null) {
+                        nv = props.getProperty(replacement);
+                    }
+                    if (nv != null) {
+                        replacement = nv;
+                    } else {
+                        throw new IllegalArgumentException("Cannot resolve configuration expression '" + replacement + "' for key '" + me.getKey() + "'");
+                    }
+                }
+                if (! group.equals(replacement)) {
+                    result = StringUtils.replace(result, group, replacement);
+                    matcher = pattern.matcher(result);
+                    replaceCount++;
+                    offset = offset + replacement.length() -1;
+                }
+            }
+            me.setValue(result);
+        } // for
+        return replaceCount;
+    }
 }
