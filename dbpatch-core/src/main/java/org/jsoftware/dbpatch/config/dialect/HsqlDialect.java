26f41a3733b47ed2a6830a761ca208795293bfd4
m-szalik <szalik@jsoftware.org>
2016-03-22 12:29:47 +0100

    HsqlDB dialect

---
+++ dbpatch-core/src/main/java/org/jsoftware/dbpatch/config/dialect/HsqlDialect.java
@@ -0,0 +1,33 @@
+package org.jsoftware.dbpatch.config.dialect;
+
+import org.jsoftware.dbpatch.impl.CloseUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+
+/**
+ * HsqlDb dialect
+ *
+ * @author szalik
+ */
+public class HsqlDialect extends DefaultDialect {
+    private static final long serialVersionUID = -1090776645312248L;
+
+    public Timestamp getNow(Connection con) throws SQLException {
+        Statement stm = null;
+        ResultSet rs = null;
+        try {
+            stm = con.createStatement();
+            rs = stm.executeQuery("VALUES (CURRENT_TIMESTAMP)");
+            rs.next();
+            return rs.getTimestamp(1);
+        } finally {
+            CloseUtil.close(rs);
+            CloseUtil.close(stm);
+        }
+    }
+
+}
